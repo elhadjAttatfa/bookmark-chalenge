4da0fecacfd3035c963fa77806fbb0606e4082a9
attatfaelhadj <attatfaelhadj@yahoo.ca>
2022-10-06 03:25:00 -0400

    mis a jour

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route} from 'react-router-dom';
+import Header from "./components/header/Header";
+import Home from './components/home/Home';
+import Features from './components/home/features/Features';
+import Login from './components/login/Login';
+import Footer from "./components/footer/Footer";
+import Downloads from "./components/home/downloadsSection/Downloads";
+import Questions from "./components/home/questions/Questions";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="features" element={<Features />} />
+          <Route path="pricing" element={<Downloads/>} />
+          <Route path="contact" element={<Questions/>} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
